82eb2e5103dacaf5b68e06744d01b3abef08b66d
Pham Hieu <phamhieu1998@gmail.com>
2020-01-15 18:34:37 +0800

    add base scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,20 @@
 import "../styles/index.scss";
-import { NextPage } from 'next'
-import { AppProps } from 'next/app'
+import { Fragment } from 'react'
+import App from 'next/app'
+import Head from 'next/head'
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+export default class MyApp extends App {
+  public render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Fragment>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700&display=swap" rel="stylesheet" />
+        </Head>
+        <Component {...pageProps} />
+      </Fragment>
+    )
+  }
 }
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
-
-export default MyApp
