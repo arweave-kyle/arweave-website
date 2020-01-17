9a7b1ce2a9094d517cdf352caeebf845e645516c
Pham Hieu <phamhieu1998@gmail.com>
2020-01-17 20:50:06 +0800

    style link

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -1,8 +1,12 @@
 import * as React from "react"
+import Link from "next/link"
 
 const NavBar: React.FunctionComponent = () => (
   <nav className="nav-main">
-    <h2>Arweave</h2>
+    <h3>arweave.org</h3>
+    <Link href="/">
+      <a className="btn primary">try for free</a>
+    </Link>
   </nav>
 )
 
