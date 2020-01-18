00ddd648989cc3a9b8532bdc6ae568ff10de8879
Pham Hieu <phamhieu1998@gmail.com>
2020-01-18 10:44:48 +0800

    fixes mobile view

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -5,7 +5,10 @@ const NavBar: React.FunctionComponent = () => (
   <nav className="nav-main">
     <h3>arweave.org</h3>
     <Link href="/">
-      <a className="btn primary">try for free</a>
+      <a className="btn primary desktop">try for free</a>
+    </Link>
+    <Link href="/">
+      <a className="btn mobile">try now</a>
     </Link>
   </nav>
 )
