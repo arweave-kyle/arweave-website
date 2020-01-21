4198e4bd1a43a5dec4502fdf693002a3acf106b6
Lili <li.saengsanthitam@gmail.com>
2020-01-21 17:22:49 +0100

    update favicon

--- components/UI/Layout.tsx
+++ components/UI/Layout.tsx
@@ -12,14 +12,14 @@ type Props = {
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
-  title = 'Arweave',
-  desc = 'Arweave is a global, permanent hard drive built on two novel technologies: the blockweave, a derivative of the blockchain, and proof of access, a custom incentivised proof of work algorithm. These innovations provide truly permanent data storage for the very first time and at a massive scale.'
+  title = 'arweave',
+  desc = 'arweave is a global, permanent hard drive built on two novel technologies: the blockweave, a derivative of the blockchain, and proof of access, a custom incentivised proof of work algorithm. These innovations provide truly permanent data storage for the very first time and at a massive scale.'
 }) => {
 
   return (
     <div className="layout">
       <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon-183x183.png" />
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 
 const NavBar: React.FunctionComponent = () => (
   <nav className="nav-main">
+    <img src="/favicon-183x183.png" />
     <h3>arweave.org</h3>
     <Link href="/">
       <a className="btn primary desktop">GET FREE TOKEN</a>
