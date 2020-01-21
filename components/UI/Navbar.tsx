937a04b11bbead0187356bef95e53f68deaa8059
Lili <li.saengsanthitam@gmail.com>
2020-01-21 17:40:04 +0100

    fix logo on top nav

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -3,8 +3,9 @@ import Link from "next/link"
 
 const NavBar: React.FunctionComponent = () => (
   <nav className="nav-main">
-    <img src="/favicon-183x183.png" />
-    <h3>arweave.org</h3>
+    <Link href="/">
+      <a className="nav-logo"><img src="/nav-logo.svg" /></a>
+    </Link>
     <Link href="/">
       <a className="btn primary desktop">GET FREE TOKEN</a>
     </Link>
