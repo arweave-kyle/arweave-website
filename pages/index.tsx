3076b1edbfeef5a6e4b530fc6ead2c64430ce8b9
Lili <li.saengsanthitam@gmail.com>
2020-01-22 13:55:32 +0100

    aos for front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,26 +16,26 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <HexagonBackground />
-      <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "1500" }} title="Store data, permanently"
+      <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} title="Store data, permanently"
         desc="Arweave enables you to permanently host  your web apps and pages, simply and quickly."
         action="SEE DOCUMENTATION" />
       <TitleSection title="Friends and partners" className="images-grid">
-        <div data-aos="fade-up"><img src="https://via.placeholder.com/200x80.png" /></div>
-        <div data-aos="fade-up"><img src="https://via.placeholder.com/100x80.png" /></div>
-        <div data-aos="fade-up"><img src="https://via.placeholder.com/100x80.png" /></div>
-        <div data-aos="fade-up"><img src="https://via.placeholder.com/100x80.png" /></div>
-        <div data-aos="fade-up"><img src="https://via.placeholder.com/100x80.png" /></div>
+        <div><img src="https://via.placeholder.com/200x80.png" /></div>
+        <div><img src="https://via.placeholder.com/100x80.png" /></div>
+        <div><img src="https://via.placeholder.com/100x80.png" /></div>
+        <div><img src="https://via.placeholder.com/100x80.png" /></div>
+        <div><img src="https://via.placeholder.com/100x80.png" /></div>
       </TitleSection>
-      <CardLinkSection aosConfig={{ "data-aos": "fade-up" }} align="left" title="Feature Number One"
+      <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} align="left" title="Feature Number One"
         desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         action="DOWNLOAD NOW" />
-      <CardLinkSection align="right" title="Feature Number Two"
+      <CardLinkSection aosConfig={{ "data-aos": "fade-up", "data-aos-easing": "linear", "data-aos-duration": "500" }} align="right" title="Feature Number Two"
         desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         action="GO TO THAT PAGE" />
       <TitleSection title="Latest from the team" className="images-grid fill">
-        <div><img src="https://via.placeholder.com/300x320.png" /></div>
-        <div><img src="https://via.placeholder.com/300x320.png" /></div>
-        <div><img src="https://via.placeholder.com/300x320.png" /></div>
+        <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="500"><img src="https://via.placeholder.com/300x320.png" /></div>
+        <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="700"><img src="https://via.placeholder.com/300x320.png" /></div>
+        <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="900"><img src="https://via.placeholder.com/300x320.png" /></div>
       </TitleSection>
       <NewsletterSignup />
     </Layout>
