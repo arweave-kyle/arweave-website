08d7ac08cc58497bcdfd7ffce296dcac236de4df
Pham Hieu <phamhieu1998@gmail.com>
2020-02-11 20:13:23 +0800

    link get free tokens page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const NavBar: React.FunctionComponent = () => (
   <nav className="nav-main">
     <a href="/" className="nav-logo"><img src="/nav-logo.svg" /></a>
-    <a href="/" className="btn primary desktop">GET FREE TOKEN</a>
+    <a href="/tokens" className="btn primary desktop">GET FREE TOKEN</a>
     <a href="/" className="btn mobile">FREE TOKEN</a>
   </nav>
 )
