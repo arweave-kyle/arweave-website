caa2b578a51f352aff2e89fbddd552a721014c2e
Pham Hieu <phamhieu1998@gmail.com>
2020-02-13 08:30:48 +0800

    clean up cookies banner

--- components/CookiesBanner.tsx
+++ components/CookiesBanner.tsx
@@ -2,26 +2,6 @@ import * as React from "react"
 import Cookies from "js-cookie"
 import Link from "next/link"
 
-function renderDefault(onApprove: () => void, onDeny: () => void) {
-  return (
-    <div className="cookies-banner__inner">
-      <div className="cookies-banner__content">
-        <h3>Cookie helps us improve your experience.</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div>
-          <Link href="/cookies-policy">
-            <a>More about our cookies policy</a>
-          </Link>
-        </div>
-      </div>
-      <div className="cookies-banner__actions">
-        <button className="primary" onClick={onApprove}>ACCEPT</button>
-        <button className="secondary" onClick={onDeny}>NO COOKIES</button>
-      </div>
-    </div>
-  )
-}
-
 const CookiesBanner: React.FunctionComponent = () => {
   const [shouldRender, setShouldRender] = React.useState(true);
   const [isOpen, setIsOpen] = React.useState(false);
@@ -55,7 +35,21 @@ const CookiesBanner: React.FunctionComponent = () => {
 
   return (
     <div className={`cookies-banner ${!isOpen ? "closed" : ""}`}>
-      {renderDefault(onApprove, onDeny)}
+      <div className="cookies-banner__inner">
+        <div className="cookies-banner__content">
+          <h3>Cookie helps us improve your experience.</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <div>
+            <Link href="/cookies-policy">
+              <a>More about our cookies policy</a>
+            </Link>
+          </div>
+        </div>
+        <div className="cookies-banner__actions">
+          <button className="primary" onClick={onApprove}>ACCEPT</button>
+          <button className="secondary" onClick={onDeny}>NO COOKIES</button>
+        </div>
+      </div>
     </div>
   )
 }
