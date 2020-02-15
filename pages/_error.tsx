32460c37c157d5b405d1bb2d60a2cdf8c337e2c2
Pham Hieu <phamhieu1998@gmail.com>
2020-02-15 18:21:03 +0800

    style error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from 'next';
+import Layout from '../components/ui/Layout'
 
 const Error: NextPage = () => {
   return (
-    <p>
-      {`An error occurred`}
-    </p>
+    <Layout className="error404">
+      <h1>404</h1>
+      <h2>PAGE NOT FOUND</h2>
+      <p>Sorry, the page you’re looking for cannot be found!</p>
+      <a className="btn" href="/">
+        BACK TO HOME
+      </a>
+    </Layout>
   )
 }
 
