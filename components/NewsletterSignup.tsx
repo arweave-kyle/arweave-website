37f006ed7d5087133a058b4890a271432633c3ce
Pham Hieu <phamhieu1998@gmail.com>
2020-02-16 06:34:13 +0800

    custom email validation tooltip

--- components/NewsletterSignup.tsx
+++ components/NewsletterSignup.tsx
@@ -1,11 +1,31 @@
-import * as React from "react"
+import { createRef, useState } from "react"
+import Validator from "validator"
+import ReactTooltip from "react-tooltip"
 
 const NewsletterSignup: React.FunctionComponent = ({
 }) => {
+  const [email, setEmail] = useState("");
+  const [validationMessage, setValidationMessage] = useState("");
+  const emailInputRef = createRef<HTMLInputElement>();
+
+  function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    const target = evt.target;
+    setEmail(target.value);
+  }
+
+  function onFocus(evt: React.FocusEvent<HTMLInputElement>) {
+    ReactTooltip.hide(emailInputRef.current!);
+  }
+
   function onSubmit(evt: React.FormEvent) {
     if (evt) evt.preventDefault();
 
-    // TODO
+    const emailValidation = Validator.isEmail(email);
+    if (!emailValidation) {
+      setValidationMessage("Require a valid email to continue.");
+      ReactTooltip.show(emailInputRef.current!);
+      return;
+    }
   }
 
   return (
@@ -14,8 +34,11 @@ const NewsletterSignup: React.FunctionComponent = ({
         <div className="newsletter-signup__content">
           <h1>Stay up to date with arweave</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-          <form onSubmit={onSubmit}>
-            <input type="email" placeholder="enter email" />
+          <form onSubmit={onSubmit} noValidate>
+            <input type="email" ref={emailInputRef} data-tip placeholder="enter email" value={email} onChange={onChange} onFocus={onFocus} />
+            <ReactTooltip place="bottom" type="error" effect="solid" event="no-event">
+              <span>{validationMessage}</span>
+            </ReactTooltip>
             <button className="primary" type="submit" >Subscribe</button>
           </form>
         </div>
