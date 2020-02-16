cc05c381d0293bd2e71c33dcdb571b9fa3533d7f
Pham Hieu <phamhieu1998@gmail.com>
2020-02-16 08:57:54 +0800

    add subcribe newsletter success message

--- components/NewsletterSignup.tsx
+++ components/NewsletterSignup.tsx
@@ -4,8 +4,10 @@ import Validator from "validator"
 const NewsletterSignup: React.FunctionComponent = ({
 }) => {
   const [email, setEmail] = useState("");
-  const [isValidEmail, setIsValidEmail] = useState(false);
   const [validationMessage, setValidationMessage] = useState("");
+
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isSucess, setIsSucess] = useState(false);
 
   function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
     const target = evt.target;
@@ -27,23 +29,36 @@ const NewsletterSignup: React.FunctionComponent = ({
       setValidationMessage("! Require a valid email to continue.");
       return;
     }
+
+    setTimeout(() => { setIsSucess(true) }, 2000);
+  }
+
+  function renderThankyou() {
+    return (
+      <div className="newsletter-signup__content">
+        <h1>Thank you for subscribing to arweave’s newsletter! </h1>
+        <p className="input-readonly-style">Email: kyles@arweave.org</p>
+      </div>
+    )
   }
 
   return (
     <div className="newsletter-signup">
       <div className="newsletter-signup__inner">
-        <div className="newsletter-signup__content">
-          <h1>Stay up to date with arweave</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-          <form className={validationMessage !== "" ? "error" : ""} onSubmit={onSubmit} noValidate>
-            <div className="input-container">
-              <input type="email" placeholder="enter email" value={email} onChange={onChange} onFocus={onFocus} />
-              {isValidEmail && <img src="/images/icons/check-circle.svg" />}
-            </div>
-            <button className="primary" type="submit" >Subscribe</button>
-            {validationMessage !== "" && <p className="error">{validationMessage}</p>}
-          </form>
-        </div>
+        {isSucess ? renderThankyou() : (
+          <div className="newsletter-signup__content">
+            <h1>Stay up to date with arweave</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+            <form className={validationMessage !== "" ? "error" : ""} onSubmit={onSubmit} noValidate>
+              <div className="input-container">
+                <input type="email" placeholder="enter email" value={email} onChange={onChange} onFocus={onFocus} />
+                {isValidEmail && <img src="/images/icons/check-circle.svg" />}
+              </div>
+              <button className="primary" type="submit" >Subscribe</button>
+              {validationMessage !== "" && <p className="error">{validationMessage}</p>}
+            </form>
+          </div>
+        )}
       </div>
     </div>
   )
